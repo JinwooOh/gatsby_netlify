38f712c85996518e4a632706e332c37bef58a352
Jinwoo <ozi0414@gmail.com>
2018-11-19 12:12:07 -0600

    init

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,10 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
-import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Jinwoo's Blog" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/Layout';
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+        <section>
+          <div className="wrapper--post">
+            <div className="blog--title">
+              <h1>Blog</h1>
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
+
+            <div className="post">
+              {posts.map(({ node: post }) => (
+                <Link to={post.fields.slug}>
+                  <div key={post.id} className="post__card">
+                    <h3>{post.frontmatter.title}</h3>
                     {post.frontmatter && (
                       <Img sizes={post.frontmatter.cover.childImageSharp.sizes} />
                     )}
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
+
+                    <small> on {post.frontmatter.date}</small>
+                  </div>
+                </Link>
               ))}
+            </div>
           </div>
         </section>
-      </Layout>
-    )
+        </Layout>
+    );
   }
 }
 
@@ -56,13 +44,13 @@ IndexPage.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -87,4 +75,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
