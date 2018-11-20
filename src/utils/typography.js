d297995d19bf2f6646d024e487fcac1c7235a3d1
Jinwoo <ozi0414@gmail.com>
2018-11-20 11:47:00 -0600

    added typhography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography';
 import kirkham from 'typography-theme-kirkham';
 
-kirkham.baseFontSize = '16px';
+kirkham.baseFontSize = '22px';
 const typography = new Typography(kirkham);
 
 export default typography;
