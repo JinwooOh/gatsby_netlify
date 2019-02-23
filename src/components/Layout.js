30848f86da28d7ea74e92c2991eb1a95ee7e04e4
Jinwoo <ozi0414@gmail.com>
2019-02-23 17:03:39 -0600

    favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Navbar from './Navbar';
+import icon16 from '../../static/logo/favicon.ico';
 import '../css/style.css';
 
 const TemplateWrapper = ({ children }) => (
   <div className="main-wrapper">
-    <Helmet title="Jinwoo's Blog" />
+    <Helmet
+      title="Jinwoo's Blog"
+      meta={[
+        { name: 'description', content: 'Web Developer Jinwoo' },
+        {
+          name: 'keywords',
+          content: 'webdev, photography, boots, blog, algorithm, data structure, design',
+        },
+      ]}
+      link={[{ rel: 'shortcut icon', type: 'image/png', href: `${icon16}` }]}
+    />
     <Navbar />
     <div className="main-container">{children}</div>
   </div>
